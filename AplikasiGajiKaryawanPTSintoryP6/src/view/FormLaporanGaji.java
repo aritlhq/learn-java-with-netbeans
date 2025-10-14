8953969915b11e82af1504eb3bb16cf5f40e36d1
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-10-15 03:55:28 +0700

    p3

---
+++ AplikasiGajiKaryawanPTSintoryP6/src/view/FormLaporanGaji.java
@@ -0,0 +1,90 @@
+package view;
+
+import controller.GajiController;
+
+public class FormLaporanGaji extends javax.swing.JInternalFrame {
+
+    private final GajiController gajiController = new GajiController();
+
+    public FormLaporanGaji() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        ruangComboBox = new javax.swing.JComboBox<>();
+        cetakButton = new javax.swing.JButton();
+        tutupButton = new javax.swing.JButton();
+
+        setClosable(true);
+        setTitle("Laporan Gaji");
+
+        jLabel1.setText("Ruang");
+
+        cetakButton.setText("Cetak");
+        cetakButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cetakButtonActionPerformed(evt);
+            }
+        });
+
+        tutupButton.setText("Tutup");
+        tutupButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                tutupButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(30, 30, 30)
+                                .addComponent(jLabel1)
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addComponent(cetakButton)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED,
+                                                        javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                .addComponent(tutupButton))
+                                        .addComponent(ruangComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 200,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addContainerGap(30, Short.MAX_VALUE)));
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(30, 30, 30)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(jLabel1)
+                                        .addComponent(ruangComboBox, javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(cetakButton)
+                                        .addComponent(tutupButton))
+                                .addContainerGap(30, Short.MAX_VALUE)));
+
+        pack();
+    }// </editor-fold>
+
+    private void tutupButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        dispose();
+    }
+
+    private void cetakButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        gajiController.cetakLaporan(ruangComboBox);
+    }
+
+    // Variables declaration - do not modify
+    private javax.swing.JButton cetakButton;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JComboBox<String> ruangComboBox;
+    private javax.swing.JButton tutupButton;
+    // End of variables declaration
+}
