f60d434988f7695e096f40b097bdc9af39d0e998
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-10-15 02:16:50 +0700

    abc

--- AplikasiGajiKaryawanPTSintoryP1/src/view/FormUtama.java
+++ AplikasiGajiKaryawanPTSintoryP1/src/view/FormUtama.java
@@ -30,6 +30,7 @@ public class FormUtama extends javax.swing.JFrame {
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
         jMenuItem2 = new javax.swing.JMenuItem();
+        jSeparator1 = new javax.swing.JPopupMenu.Separator();
         jMenuItem3 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
         jMenu3 = new javax.swing.JMenu();
@@ -40,10 +41,16 @@ public class FormUtama extends javax.swing.JFrame {
         jMenu1.setText("Aplikasi");
 
         jMenuItem1.setText("Deskripsi");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
         jMenu1.add(jMenuItem1);
 
         jMenuItem2.setText("Login");
         jMenu1.add(jMenuItem2);
+        jMenu1.add(jSeparator1);
 
         jMenuItem3.setText("Keluar");
         jMenu1.add(jMenuItem3);
@@ -53,7 +60,7 @@ public class FormUtama extends javax.swing.JFrame {
         jMenu2.setText("Master Data");
         jMenuBar1.add(jMenu2);
 
-        jMenu3.setText("Transaksi");
+        jMenu3.setText("Trabsaksi");
         jMenuBar1.add(jMenu3);
 
         jMenu4.setText("Laporan");
@@ -65,15 +72,19 @@ public class FormUtama extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 470, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 277, Short.MAX_VALUE)
+            .addGap(0, 355, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -119,5 +130,6 @@ public class FormUtama extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JPopupMenu.Separator jSeparator1;
     // End of variables declaration//GEN-END:variables
 }
